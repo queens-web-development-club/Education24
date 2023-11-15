bcf5e8155ca7945ceff0036e60f63abc503730fa
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2023-11-15 16:18:56 -0500

    Style UpcomingEventCard.js in club3

--- week5/lecture_files/club3/src/components/UpcomingEventCard.js
+++ week5/lecture_files/club3/src/components/UpcomingEventCard.js
@@ -2,41 +2,40 @@ import Calendar from "../img/icons/calendarIcon.png";
 
 function UpcomingEventCard(props) {
   return (
-    <div className="upcoming-event-card">
-      <p className="card-title">{props.info.name}</p>
-      <div className="event-details">
+    <div className="m-5 w-11/12 rounded-lg bg-white p-5 text-yellow-700">
+      <p className="pl-5 pt-5 text-6xl font-bold">{props.info.name}</p>
+      <div className="flex p-2.5">
         {/* Left */}
-        <div className="event-details__left">
-          <p>{props.info.description}</p>
-          <div className="all-details">
-            <div className="calendar-data">
-              <span className="month">{props.info.month}</span>
-              <span className="day">{props.info.day}</span>
+        <div className="w-3/4 p-4 ">
+          <p className="pb-8 text-3xl">{props.info.description}</p>
+          <div className="flex">
+            <div className="flex h-32 w-32 flex-col items-center justify-center rounded-2xl bg-lime-500">
+              <span className="text-2xl text-white">{props.info.month}</span>
+              <span className="text-7xl text-white">{props.info.day}</span>
             </div>
-            <div className="details">
-              <span>{props.info.time}</span>
-              <span>{props.info.location}</span>
-              <span>{props.info.price}</span>
+            <div className="ml-5 flex flex-col">
+              <span className="pt-1.5 text-3xl">{props.info.time}</span>
+              <span className="pt-1.5 text-3xl">{props.info.location}</span>
+              <span className="pt-1.5 text-3xl">{props.info.price}</span>
             </div>
           </div>
         </div>
 
         {/* Right */}
-        <div className="event-details__right">
-          <div className="event-details__right__row1">
+        <div className="mt-auto flex w-1/4 flex-col p-4">
+          <div className="flex justify-between pb-4 text-3xl">
             <p>Spaces Left</p>
             <span>{props.info.spaces_left}</span>
           </div>
-          <div className="event-details__right__row2">
-            <p>Registerd</p>
+          <div className="flex justify-between pb-6 text-3xl">
+            <p>Registered</p>
             <span>{props.info.registered}</span>
           </div>
-          <div className="event-details__right__row3">
-            <button className="register-button">Register</button>
-            <button
-              className="calendar-button"
-              style={{ "align-items": "center", display: "flex" }}
-            >
+          <div className="flex justify-between text-3xl">
+            <button className="rounded-lg bg-lime-500 px-8 py-5 text-white transition-colors duration-300 hover:bg-lime-600">
+              Register
+            </button>
+            <button className="flex h-20 items-center rounded-lg bg-lime-500 p-5 hover:bg-lime-600">
               <img src={Calendar}></img>
             </button>
           </div>
