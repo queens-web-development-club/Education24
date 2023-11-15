d8fe6d8603d08e3b808fbbdeab9d560944b301c9
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2023-11-15 16:20:15 -0500

    Reset data for club3 and club4 to initial states

--- week5/lecture_files/club4/src/pages/UpcomingEvents.js
+++ week5/lecture_files/club4/src/pages/UpcomingEvents.js
@@ -3,58 +3,58 @@ import React, { useState } from "react";
 
 // How we would pass props not using a database
 
-// const events = [];
+const events = [];
 
-const events = [
-  {
-    name: "Legget Hall Viewing",
-    description:
-      "...Literally. Come stare at people that go in and out of Leggett Hall. The registration fee goes towards the participants' popcorn fund.",
-    month: "NOV",
-    day: 15,
-    time: "3:00 PM",
-    location: "Parking Lot Behind Stirling Hall",
-    price: "$3.33",
-    spaces_left: 2,
-    registered: 3,
-  },
-  {
-    name: "Stalk-Off",
-    description:
-      "Battle royale to see who can find everyone else's addresses first. The registration fee goes towards the club leads' popcorn fund.",
-    month: "NOV",
-    day: 29,
-    time: "11:00 PM",
-    location: "Your Place of Residence",
-    price: "$6.66",
-    spaces_left: 3,
-    registered: 17,
-  },
-  {
-    name: "You Are Being Watched",
-    description:
-      "Look behind you. The registration fee goes towards the cost of your existence permit.",
-    month: "NOV",
-    day: "??",
-    time: "?:?? AM",
-    location: "Your Current Location ",
-    price: "$9.99",
-    spaces_left: "?",
-    registered: "?",
-  },
-  {
-    name: "Valentines Day Blank Stare",
-    description:
-      "You got a date? Yeah, me neither. Come stare at an assigned staring partner this Valentine's Day. Only lonely people allowed.",
-    month: "FEB",
-    day: 14,
-    time: "8:00 AM",
-    location: "136 Princess St (Shoppers Drug Mart)",
-    price: "FREE",
-    spaces_left: 39,
-    registered: 1,
-  },
-];
+// const events = [
+//   {
+//     name: "Legget Hall Viewing",
+//     description:
+//       "...Literally. Come stare at people that go in and out of Leggett Hall. The registration fee goes towards the participants' popcorn fund.",
+//     month: "NOV",
+//     day: 15,
+//     time: "3:00 PM",
+//     location: "Parking Lot Behind Stirling Hall",
+//     price: "$3.33",
+//     spaces_left: 2,
+//     registered: 3,
+//   },
+//   {
+//     name: "Stalk-Off",
+//     description:
+//       "Battle royale to see who can find everyone else's addresses first. The registration fee goes towards the club leads' popcorn fund.",
+//     month: "NOV",
+//     day: 29,
+//     time: "11:00 PM",
+//     location: "Your Place of Residence",
+//     price: "$6.66",
+//     spaces_left: 3,
+//     registered: 17,
+//   },
+//   {
+//     name: "You Are Being Watched",
+//     description:
+//       "Look behind you. The registration fee goes towards the cost of your existence permit.",
+//     month: "NOV",
+//     day: "??",
+//     time: "?:?? AM",
+//     location: "Your Current Location ",
+//     price: "$9.99",
+//     spaces_left: "?",
+//     registered: "?",
+//   },
+//   {
+//     name: "Valentines Day Blank Stare",
+//     description:
+//       "You got a date? Yeah, me neither. Come stare at an assigned staring partner this Valentine's Day. Only lonely people allowed.",
+//     month: "FEB",
+//     day: 14,
+//     time: "8:00 AM",
+//     location: "136 Princess St (Shoppers Drug Mart)",
+//     price: "FREE",
+//     spaces_left: 39,
+//     registered: 1,
+//   },
+// ];
 
 function UpcomingEvents(props) {
   const [data, setData] = useState(events);
